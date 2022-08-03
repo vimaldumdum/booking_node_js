482deb0c63f36e2c81c8a565705b4d2d6d913721
Vimal Kumar <vimal.kumar@gmail.com>
2022-08-03 16:04:30 +0530

    nodemon setup done

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import config from "config";
 
 const app = express()
 app.listen(config.get("Server.port"), ()=>{
-    console.log("Server is up on port " + port);
+    console.log("Server is up on port " + config.get("Server.port"));
 })
